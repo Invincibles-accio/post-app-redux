0130aafd5b36c4a2b493928144468e0be3b2f033
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-04-26 21:49:55 +0530

    post app complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {fetchData} from './actions';
+
+
+
+
+// dispatch  => useDispacth
+
+// store => get data from store => getState => useSelector 
+
+let post_style = {
+    border: "1px solid black",
+    margin: "10px",
+    padding: "10px",
+    bacground: "lightgray"
 }
 
-export default App;
+
+const App = () => {
+     const loading  = useSelector((state) =>state.data.loading)
+     const data  = useSelector((state) =>state.data.data)
+     const error = useSelector((state) =>state.data.error)
+    
+      const dispatch = useDispatch()
+    
+     useEffect(() =>{
+          dispatch(fetchData())
+     }, [dispatch]
+     )
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    if (error) {
+        return <h1>Error: {error}</h1>
+    }
+
+    return(
+      <div>
+           <h1>Posts</h1>
+           {
+              data.map((post, index) =>
+                  
+                 <div key={post.id} style={post_style}> 
+                      <h2>{post.title}</h2>
+                      <p>{post.body}</p>
+                 </div>
+                
+                )
+           }
+      </div>
+    )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+// state = {
+//     data: {
+ //   loading: true,
+//    data: [],
+//    error: ""
+//},
+//     
+//   }
